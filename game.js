bc717249d44f786c04059cd4e267bfd8c3a0b770
Zin Wai Htet <69818041+yatozuki@users.noreply.github.com>
2024-05-19 22:05:03 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,93 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+    
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern)
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+function nextSequence() {
+    userClickedPattern = []
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+
+}
+
+function animatePress (currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success")
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        $("#level-title").html("Game Over! <br>Press Any Key to Restart.</br> <br>Max Level " + level);
+
+        $("body").addClass("game-over")
+
+        setTimeout(function () {
+            $("body").removeClass("game-over")
+        }, 200);
+
+        var wrong = new Audio("sounds/wrong.mp3")
+        wrong.play();
+
+        startOver();
+
+        console.log("wrong");
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
